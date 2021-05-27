ba2ed0dc5f6db79d3c37bc2e81ac28f447871e50
gcxfd <i@rmw.link>
2021-05-27 12:57:46 +0800

    🔶

--- test.dart
+++ test.dart
@@ -3,7 +3,7 @@ import 'dart:typed_data';
 import 'package:path/path.dart' as path;
 import './c/blake3.dart';
 import 'dart:ffi';
-import 'dart:convert' show utf8;
+import 'dart:convert' show utf8, base64;
 import 'package:ffi/ffi.dart' as ffi;
 
 const allocate = ffi.malloc;
@@ -12,7 +12,7 @@ final DIR = path.dirname(Platform.script.toFilePath());
 extension Uint8ListPointer on Uint8List {
   // https://github.com/dart-lang/ffi/issues/31
   // Workaround: before does not allow direct pointer exposure
-  Pointer ptr() {
+  Pointer<Uint8> ptr() {
     final ptr = allocate<Uint8>(length);
     final typedList = ptr.asTypedList(length);
     typedList.setAll(0, this);
@@ -31,7 +31,11 @@ void main() {
   lib.blake3_hasher_init(hasher);
   lib.blake3_hasher_update(hasher, data.ptr().cast(), data.length);
 
-  final out = Uint8List(BLAKE3_OUT_LEN);
-  lib.blake3_hasher_finalize(hasher, out.ptr().cast(), BLAKE3_OUT_LEN);
-  print(out);
+  final out = allocate<Uint8>(BLAKE3_OUT_LEN);
+  lib.blake3_hasher_finalize(hasher, out, BLAKE3_OUT_LEN);
+  final hash = out.asTypedList(BLAKE3_OUT_LEN);
+
+  print('${hash.runtimeType} ${base64.encode(hash)}');
+  ffi.malloc.free(out);
+  ffi.malloc.free(hasher);
 }
